7f6baf39c878ce3a0d72ffc8958155436a0bc375
Bagas Ramiro <bagasramiros19@gmail.com>
2026-02-01 11:39:34 +0700

    add try catch to choose the role of character

--- Users/Asus/Kampus/semester4/latihan/PBO/code/src/practiceinheritance/Main.java
+++ Users/Asus/Kampus/semester4/latihan/PBO/code/src/practiceinheritance/Main.java
@@ -1,12 +1,53 @@
 package practiceinheritance;
+import java.util.Scanner;
 
+
+ 
 public class Main {
+enum roleGame{MAGE,ATTACKER,ARROW};   
+    void roleCharacter(){
+
+        System.out.println("Input the role");
+        Scanner rolesChar= new Scanner(System.in);
+        String userInput = rolesChar.nextLine();
+        roleGame role =null;
+
+        try{
+         role = roleGame.valueOf(userInput.toUpperCase());
+         
+        switch(role){
+            case MAGE: 
+               mainCharacter mage = new mageCharacter("Lina", 120, 25, 90);
+               mage.display(); 
+               break;
+
+            case ATTACKER:
+                mainCharacter attacker = new attackerCharacter("Rezy", 90, 110, 96);
+                attacker.display();
+                break;
+            case ARROW:
+                System.out.println("Character not avaible");
+                break;
+            default:
+                System.out.println("Invalid role");
+                break;
+        }
+    }
+
+        catch(IllegalArgumentException e){
+            System.out.println("Role invalid");
+            return;
+        }
+        
+    }
     public static void main(String[] args) {
         // mainCharacter attacker = new attackerCharacter("Ryza", 90, 80, 23);
-        // attacker.display();    
-        mainCharacter mage = new mageCharacter("Lina", 120, 25, 90);
-        mage.display();  
-             
+        // attacker.display(); 
+        Main app= new Main();   
+        app.roleCharacter();
+        
+         
+
     }
 
 }
